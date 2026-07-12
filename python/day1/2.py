4b8d1fb63c0bcbcef4f14ce6e1b9040b4e607259
NoobCodder7 <bagulviraj7@gmail.com>
2026-07-12 11:55:49 +0530

    Added file

---
+++ python/day1/2.py
@@ -0,0 +1,4 @@
+
+#program to take input and print formatted output 
+name = input("Enter your name")
+print(f"My Name is {name}")
---
+++ python/day1/operators.py
@@ -0,0 +1,14 @@
+#here is a simple program to learn about operators in python 
+
+# Arithmetic operators 
+
+a = 10
+b = 5
+
+print(a+b) # "+" is use to add two numbers
+print(a-b) # "-" is use to subract two numbers
+print(a*b) # "*" is used to multiply two numbers
+print(a/b) # "/" is use to get quotient with decimal
+print(a//b) # "//" is use to get quotient without decimal
+print(a%b) # "%" is use to get remainde
+print(a**b) # is use to get exponential 
