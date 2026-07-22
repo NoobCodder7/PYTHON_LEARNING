3b65aa99702149cca9b6247b02c317f2da727013
NoobCodder7 <bagulviraj7@gmail.com>
2026-07-22 18:54:20 +0530

    OOP DAY1

---
+++ python/Day6/Day7(OOP)/Day1(OOP)/1.py
@@ -0,0 +1,58 @@
+"""Question 1
+
+Create a class called Student.
+
+Create one object named student1.
+
+"""
+
+class Student:
+
+    pass
+
+S1 = Student()
+print(type(S1))
+
+
+
+
+"""Question 2
+Create a class called Teacher.
+Create 10 teacher objects using different variable names.
+"""
+
+class Teacher:
+    pass
+
+T1=Teacher()
+T2=Teacher()
+T3=Teacher()
+T4=Teacher()
+T5=Teacher()
+T6=Teacher()
+T7=Teacher()
+T8=Teacher()
+T9=Teacher()
+T10=Teacher()
+
+print("10 Teacher objects created successfully!")
+
+
+
+
+"""Create a class called Animal.
+
+Create these objects"""
+
+
+class Animal:
+    pass
+
+
+Cat = Animal()
+Dog = Animal()
+Bird = Animal()
+
+print(Cat)
+print(Dog)
+print(Bird)
---
+++ python/Day6/Day7(OOP)/Day1(OOP)/mini_project.py
@@ -0,0 +1,78 @@
+"""Mini Project (Day 1)
+School Management (Basic)
+
+Create these classes:
+
+Student
+Teacher
+Classroom
+Principal
+Library
+
+Now create:
+
+3 Student objects
+2 Teacher objects
+1 Classroom object
+1 Principal object
+1 Library object
+
+Print every object."""
+
+
+
+
+
+class Teacher:
+    pass
+class Student:
+    pass
+class Principal:
+    pass
+class ClassRoom:
+    pass
+class Library:
+    pass
+
+
+
+S1 = Student()
+S2 = Student()
+S3 = Student()
+
+T1 = Teacher()
+T2 = Teacher()
+
+
+P = Principal()
+
+C1 = ClassRoom()
+C2 = ClassRoom()
+
+B1 = Library()
+
+
+print(S1)
+print(S2)
+
+print(S3)
+
+print(T1)
+
+print(T2)
+
+print(P)
+
+print(C1)
+
+print(B1)
+
+
+
+
+
+
+
+
+
+
